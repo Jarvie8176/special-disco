2b870204042925270c6156b4d3f20216d07b706c
Jarvie8176 <3547456+Jarvie8176@users.noreply.github.com>
2022-01-12 19:48:40 -0500

    done

---
+++ helpers.js
@@ -0,0 +1,9 @@
+function encrypt(message = '', key = ''){
+    var message = CryptoJS.AES.encrypt(message, key);
+    return message.toString();
+}
+function decrypt(message = '', key = ''){
+    var code = CryptoJS.AES.decrypt(message, key);
+    var decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+    return decryptedMessage;
+}
